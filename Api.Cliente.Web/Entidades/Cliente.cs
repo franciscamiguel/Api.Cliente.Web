5c9bac81a7dbfa38b2933a7752fed838970ec53d
francisca92firmino <francisca.miguel@woli.tec.br>
2022-02-17 19:18:36 -0300

    Adicionada duas classes de entidades

---
+++ Api.Cliente.Web/Entidades/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+
+namespace Api.Cliente.Web.Entidades
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string RG { get; set; }
+
+        public int EnderecoId { get; set; }
+        public List<Endereco> Enderecos { get; set; }
+    }
+}
---
+++ Api.Cliente.Web/Entidades/Endereco.cs
@@ -0,0 +1,17 @@
+﻿namespace Api.Cliente.Web.Entidades
+{
+    public class Endereco
+    {
+        public int Id { get; set; }
+        public string Nome  { get; set; }
+        public string Numero  { get; set; }
+        public string Bairro  { get; set; }
+        public string Cidade  { get; set; }
+        public string UF  { get; set; }
+        public string CEP  { get; set; }
+
+        public int ClienteId  { get; set; }
+        public Cliente Cliente { get; set; }
+
+    }
+}
