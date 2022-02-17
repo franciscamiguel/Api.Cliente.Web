8f8ca21e004e00380257066a201e23b65b3e3d7b
francisca92firmino <francisca.miguel@woli.tec.br>
2022-02-17 19:04:07 -0300

    Adiconado cliente controller

---
+++ Api.Cliente.Web/Controllers/ClienteController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Cliente.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClienteController : ControllerBase
+    {
+        private readonly ILogger<ClienteController> _logger;
+
+        public ClienteController(ILogger<ClienteController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("cliente");
+        }
+
+        [HttpGet("buscar-cliente")]
+        public IActionResult BuscarCliente()
+        {
+            return Ok("cliente");
+        }
+    }
+}
